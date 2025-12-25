5804ddab156554b416afc00c6281536d61d20299
Ankit Mishra <ankitkmishra250@gmail.com>
2025-12-26 00:49:37 +0530

    Static Method

---
+++ Opp/static_method.py
@@ -0,0 +1,38 @@
+class Car:
+    total_car_created = 0
+
+    def __init__(self , name , brand , category):
+        self.name = name 
+        self.__brand = brand 
+        self.category = category
+        Car.total_car_created += 1
+    
+    def get_brand(self):
+        return f"{self.__brand}  ! " 
+    
+    @staticmethod
+    def car_description():
+        return "Car is a transport medium"
+
+class Electric_car(Car):
+    def __init__(self ,name , brand , category):
+        super().__init__(name , brand , category)
+
+    def recharging(self):
+        return "recharging through Eletricity"
+
+class Fuel_car(Car):
+    def __init__(self , name , brand , category):
+        super().__init__(name , brand , category)
+    
+    def recharging(self):
+        return "recharging through fuel"
+
+class Hybrid(Car):
+    def __init__(self , name , brand , category):
+        super().__init__(name , brand , category)
+    
+    def recharging(self):
+        return "recharging through both(feuls and electricity)"
+
+print(Car.car_description())
